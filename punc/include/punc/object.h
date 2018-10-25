effd3b8b68a7ee5ab173f7c991e405e1f77457d1
sigvaldm <sigvaldm@fys.uio.no>
2018-10-25 20:48:46 +0200

    Added documentation

--- punc/include/punc/object.h
+++ punc/include/punc/object.h
@@ -49,16 +49,51 @@ class Object {
 public:
     using size_t = std::size_t;
 
-    double charge;
-    double current;
-    double potential;
+    /**
+     * @brief The charge on the object.
+     * Incremented during Population::update(), and corrected by ::update().
+     */
+    double charge = 0;
+
+    //! Current collected during last Population::update().
+    double current = 0;
+
+    //! Latest known potential on the object.
+    double potential = 0; 
+
+    //! Boundary facet function id
     size_t bnd_id;
 
+    /**
+     * @brief Constructor
+     */
     Object(size_t bnd_id) : bnd_id(bnd_id) {};
+
+    /**
+     * @brief Apply any associated boundary conditions (if any) to vector.
+     * @param[in, out]  b   Vector
+     */
     virtual void apply(df::GenericVector &b){};
+
+    /**
+     * @brief Apply any associated boundary conditions (if any) to matrix.
+     * @param[in, out]  A   Matrix
+     */
     virtual void apply(df::GenericMatrix &A){};
+
+    /**
+     * @brief Update the object based on the newest potential.
+     * @param           phi     Correct potential
+     *
+     * Some methods do not know the correct charge and potential of the object
+     * before after the potential has been solved for. Given the correct
+     * a-posteriori potential the charge and potential of the object is
+     * corrected.
+     */
     virtual void update(const df::Function &phi) = 0;
 };
+
+//! A polymorphic vector of Objects.
 using ObjectVector = std::vector<std::shared_ptr<Object>>;
 
 class Circuit {
@@ -69,10 +104,33 @@ public:
     // std::vector<std::vector<int>> groups;
     // double dt;
     // double eps0;
+
+    /**
+     * @brief Check if selected solvers are valid or assign default solvers.
+     * @param[in, out]  method          Linear algebra solver.
+     * @param[in, out]  preconditioner  Linear algebra preconditioner.
+     * @return                          Whether the choice is valid.
+     *
+     * Not all combinations of methods and preconditioners may work for all
+     * object methods. This method returns true for combinations that are
+     * known to work. Combinations which (sometimes) fail, or are untested
+     * return false. If method and preconditioner are empty strings, the
+     * method fills them with the suggested methods and returns true.
+     * The methods used are the ones documented in FEniCS.
+     */
     virtual bool check_solver_methods(std::string &method,
                                       std::string &preconditioner) const = 0;
 
+    /**
+     * @brief Apply any associated boundary conditions (if any) to vector.
+     * @param[in, out]  b   Vector
+     */
     virtual void apply(df::GenericVector &b){};
+
+    /**
+     * @brief Apply any associated boundary conditions (if any) to matrix.
+     * @param[in, out]  A   Matrix
+     */
     virtual void apply(df::PETScMatrix &A){};
 };
 
